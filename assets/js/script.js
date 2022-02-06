a05915a5b9b64916d09c9f60f2c3b7798f6cdfad
Melvin Welton <nivlem65@gmail.com>
2022-02-06 17:35:05 -0600

    commit on main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,53 @@
-// write a fetch request for weather api
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}')
+  
+var apiKey = "30865bc8191fe2edd17e05798968b961";
+var lat = "";
+var lon = "";
+var city = "";
 
-    .then(function (response) {
-        return response.json();
+
+
+var form = document.querySelector(".container form");
+
+
+$("#select-city").on("click", function (event) {
+    event.preventDefault();
+    city = $("#city-input").val().trim();
+    var textContent = $(this).siblings("input").val();
+    var storeArr = [];
+    storeArr.push(textContent);
+    localStorage.setItem('cityName', JSON.stringify(storeArr))
+    console.log(storeArr[0]);
+
+    findCity(city);
+});
+
+function findCity(){
+    var WUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
+    
+    
+    $.ajax({
+        url: WUrl,
+        method: 'GET'
+    }).then(function (data) {
+        console.log(data);
+        console.log(WUrl);
+        $("#current").empty();
+
+        lat = data.coord.lat;
+        lon = data.coord.lon;
+        findLatLon(lat,lon);
+        console.log(lat);
     })
 
-    .then(function (data) {
+}
+
+function findLatLon(){
+    var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid="+apiKey;
+
+    $.ajax({
+        url:oneCallUrl,
+        method:'GET'
+    }).then(function(data){
         console.log(data);
-    });
+    })
+}
